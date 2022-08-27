4226d8a93cdbf772b8df98561932470cd3accb85
Atul Kumar Hande <handeatul03a@gmail.com>
2022-08-28 00:42:22 +0530

    feat: added basic middleware and connected to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import express from "express";
+import connectDB from "./db/connect.js";
 const app = express();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+//middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const port = process.env.PORT || 5000;
 
@@ -7,6 +15,21 @@ app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-app.listen(port, () => {
-  console.log(`server listening at port ${port}`);
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(port, () => {
+      console.log(`server listening at port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
