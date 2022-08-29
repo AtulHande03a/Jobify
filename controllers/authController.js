3bfc64e8e5d432d5d2f826efcc49f94c4de9caa5
Atul Kumar Hande <handeatul03a@gmail.com>
2022-08-29 17:25:19 +0530

    feat: added error handle checks in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,22 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all fields");
+  }
+
+  //check if email exist
+  const useAlreadyExists = await User.findOne({ email });
+
+  if (useAlreadyExists) {
+    throw new BadRequestError("email already in use");
+  }
+
+  const user = await User.create({ email, name, password });
   res.status(StatusCodes.OK).json({ user });
 };
 
