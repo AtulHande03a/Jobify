8d719ef979f4ec8b3eb40a52863da2ba4a5e3048
Atul Kumar Hande <handeatul03a@gmail.com>
2022-08-31 23:23:38 +0530

    feat:added navbar structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,44 @@
 import Wrapper from "../assets/wrappers/Navbar.js";
-import {FaHome} from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext.js";
+import Logo from "./Logo.js";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome/> 
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => console.log("toggle-sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("show/hide dropdown")}
+          >
+            <FaUserCircle />
+            Atul
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              className="dropdown-btn"
+              type="button"
+              onClick={() => console.log("logout-user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
