d942f9f52e429af4fb444e6a36265f4f126cd9b0
Atul Kumar Hande <handeatul03a@gmail.com>
2022-09-07 16:04:41 +0530

    feat: added stats logic on server side

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,6 +2,7 @@ import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, NotFoundError } from "../errors/index.js";
 import checkPermissions from "../utils/checkPermission.js";
+import mongoose from "mongoose";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -63,7 +64,24 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  await res.send("stats shown");
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  res.status(StatusCodes.OK).json({ stats });
 };
 
 export { getAllJobs, createJob, deleteJob, updateJob, showStats };
